4cf21f60c6d8079c4a95c5978673769a42ff6d7b
Guilherme Ribeiro <guiribeiro1108@gmail.com>
2025-11-27 01:13:38 -0300

    Create gerador.py

---
+++ jogo_digitacao/gerador.py
@@ -0,0 +1,9 @@
+def gerar_texto(tipo_teclado="pt"):
+    if tipo_teclado == "pt":
+        return "A maçã está fresca, café e pão são deliciosos."
+    
+    elif tipo_teclado == "en":
+        return "A maca esta fresca, cafe e pao sao deliciosos."
+    
+    else:
+        return "Texto padrão."
