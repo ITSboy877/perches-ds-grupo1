dc166f0f22c2cbe2213f1304c72268c9f1b8b66f
Guilherme Ribeiro <guiribeiro1108@gmail.com>
2025-11-27 01:17:39 -0300

    Create interface.py

---
+++ jogo_digitacao/interface.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+import config
+
+class Interface:
+    def __init__(self, root, tipo_teclado):
+        self.root = root
+        self.tipo_teclado = tipo_teclado 
+        self.label_texto = tk.Label(root, font = config.FONTE, fg = config.COR_TEXTO, bg = config.COR_FUNDO)
+        self.label_texto.pack(pady = 20)
+
+        self.caixa_digitacao = tk.Entry(root, font=config.FONTE)
+        self.caixa_digitacao.pack(pady = 10)
+        self.caixa_digitacao.bind("<KeyRelease>", self.tecla_pressionada)
+
+        self.texto = ""
+
+    def definir_texto(self, texto):
+        self.texto = texto
+        self.label_texto.config(text=self.texto)
+        self.caixa_digitacao.delete(0, tk.END)
+
+    def tecla_pressionada(self, event):
+        digitado = self.caixa_digitacao.get()
+        
+        if self.tipo_teclado == "pt":
+            pass
+
+        elif self.tipo_teclado == "en":
+            pass
+
+        print(f"Digitado: {digitado}")
+
+    def iniciar(self):
+        self.caixa_digitacao.focus_set()
