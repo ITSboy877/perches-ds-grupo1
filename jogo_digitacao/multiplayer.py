d54712639e70ea54462a98afa144eaa939080020
Guilherme Ribeiro <guiribeiro1108@gmail.com>
2026-02-12 08:22:53 -0300

    atualização

--- jogo_digitacao/multiplayer.py
+++ jogo_digitacao/multiplayer.py
@@ -8,6 +8,7 @@ class SessaoMultiplayer:
         self.jogador_atual = 1
     
     def adicionar_resultado(self, nome_jogador, pontos, wpm, precisao, tempo):
+        """Adiciona resultado de um jogador"""
         self.resultados.append({
             "jogador": nome_jogador,
             "numero": self.jogador_atual,
@@ -19,12 +20,15 @@ class SessaoMultiplayer:
         self.jogador_atual += 1
     
     def sessao_completa(self):
+        """Verifica se todos já jogaram"""
         return len(self.resultados) >= self.num_jogadores
     
     def obter_vencedor(self):
+        """Retorna o vencedor da sessão"""
         if not self.resultados:
             return None
         return max(self.resultados, key=lambda x: x["pontos"])
     
     def obter_ranking(self):
+        """Retorna ranking completo ordenado"""
         return sorted(self.resultados, key=lambda x: x["pontos"], reverse=True)
